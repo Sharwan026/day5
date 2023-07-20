d49b26503cd2237e90394d7fc2503000f5079064
Sharwan026 <113581881+Sharwan026@users.noreply.github.com>
2023-07-20 09:23:02 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,37 @@
+// import React from "react";
+// import Navbar from "./navbar";
+// import './home.css';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+//  function Home(){
+//     return(
+//         <div className="ab">
+// <div>
+// <Navbar/>   
+// </div>
+// <div>
+ 
+// </div>
+//         </div>
+//     );
+// }
+// export default Home;  
+import React from "react";
+import Navbar from "./navbar";
+import './home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+     
+
+     
+    </div>
+  );
+}
+
+export default Home;
